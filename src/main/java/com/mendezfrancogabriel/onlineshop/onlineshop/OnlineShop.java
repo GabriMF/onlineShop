3985223469e6bb5e5712305b4aaa1ef6959c8c84
Gabriel <gabrimf25@gmail.com>
2025-01-31 13:29:38 +0100

    online shop project created

---
+++ src/main/java/com/mendezfrancogabriel/onlineshop/onlineshop/OnlineShop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mendezfrancogabriel.onlineshop.onlineshop;
+
+/**
+ *
+ * @author alu15d
+ */
+public class OnlineShop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
